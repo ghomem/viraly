8cdaa1c9062870c581c748a4c855eb8199600cd8
Gustavo Homem <Gustavo Homem>
2020-05-21 15:23:42 +0100

    sync production with staging version: separate smaller plots + infection time stdev

--- web/README.md
+++ web/README.md
@@ -6,7 +6,14 @@ This is a bokeh frontend for the viraly script. It can be launched with:
 MYDIR=`dirname $0`
 
 cd $MYDIR
-bokeh serve --allow-websocket-origin=lo.gic.li  viral.py
+bokeh serve --allow-websocket-origin=lo.gic.li viral.py
 ```
 
 For production use an nginx (or equivalent reverse proxy) should be put in front of the web application.
+
+Q: Why is there a viral-staging.py file here? Do I not know that a branch could be used for staging?
+A: It is because it is because it is practical to run both staging and production in parallel on the same bokeh server using two nginx locations, the same repo and the same bokeh process.
+
+```
+bokeh serve --allow-websocket-origin=lo.gic.li viral.py viral-staging.py
+```
--- web/viral.py
+++ web/viral.py
@@ -17,10 +17,11 @@ from viraly import *
 ### Configuration
 
 # Geometry and visuals
-PLOT_TOOLS  ='save,reset,pan,wheel_zoom,box_zoom'
-PLOT_HEIGHT = 400
-PLOT_WIDTH  = 600
-TEXT_WIDTH = 300
+PLOT_TOOLS    ='save,reset,pan,wheel_zoom,box_zoom'
+PLOT_HEIGHT   = 300
+PLOT_WIDTH    = 500
+TEXT_WIDTH    = 300
+LMARGIN_WIDTH = 20
 
 PLOT_LINE_WIDTH = 3
 PLOT_LINE_ALPHA = 0.6
@@ -54,7 +55,11 @@ IIF_START = 4
 T_MIN   = 7
 T_MAX   = 50
 T_START = 19
-T_STDEV = 2  # this is currently hardcoded, not sure a slider for this adds that much value
+
+# and its Standard Deviation
+T_STDEV_MIN   = 0
+T_STDEV_MAX   = 3
+T_STDEV_START = 0
 
 # Incubation period
 I_MIN   = 1
@@ -108,26 +113,30 @@ DRATE_STEP  = 0.25
 # labels and strings
 PAGE_TITLE  ='3 stage epidemic simulator'
 PLOT_TITLE  ='Active'
-PLOT2_TITLE ='New, Recovered, Dead'
+PLOT2_TITLE ='New, Recovered'
 PLOT3_TITLE ='Rt estimation'
 PLOT4_TITLE ='Immunity'
+PLOT5_TITLE ='Dead'
+PLOT6_TITLE ='Accumulated cases / recoveries'
+PLOT7_TITLE ='Accumulated deaths'
 
-T_LABEL     = 'Infectious Period'
-I_LABEL     = 'Incubation Period'
-POP_LABEL   = 'Population (Millions)'
-IIF_LABEL   = 'Initial number of infections'
-DUR1_LABEL  = 'Free expansion duration'
-DUR2_LABEL  = 'Confinement duration (including transition)'
-TRA1_LABEL  = 'Transition to confinement duration'
-TRA2_LABEL  = 'Transition to deconfinement duration'
-BETA1_LABEL = 'Beta during free expansion (x10)'
-BETA2_LABEL = 'Beta during confinement (x10)'
-BETA3_LABEL = 'Beta after confinement (x10)'
-DRATE_LABEL = 'Death rate (%)'
+T_LABEL       = 'Infectious Period'
+T_STDEV_LABEL = 'Infectious Period Standard Deviation'
+I_LABEL       = 'Incubation Period'
+POP_LABEL     = 'Population (Millions)'
+IIF_LABEL     = 'Initial number of infections'
+DUR1_LABEL    = 'Free expansion duration'
+DUR2_LABEL    = 'Confinement duration (including transition)'
+TRA1_LABEL    = 'Transition to confinement duration'
+TRA2_LABEL    = 'Transition to deconfinement duration'
+BETA1_LABEL   = 'Beta during free expansion (x10)'
+BETA2_LABEL   = 'Beta during confinement (x10)'
+BETA3_LABEL   = 'Beta after confinement (x10)'
+DRATE_LABEL   = 'Death rate (%)'
 
-TEXT_INTRO   = 'Use the mouse for initial selection and cursors for fine tuning:'
-TEXT_SUMMARY = 'Stats:'
-TEXT_NOTES   ='<b>Notes:</b><br/>\
+TEXT_INTRO    = 'Use the mouse for initial selection and cursors for fine tuning:'
+TEXT_SUMMARY  = 'Stats:'
+TEXT_NOTES    ='<b>Notes:</b><br/>\
               &bull; Confinement phase is highlighted in red.<br/>\
               &bull; Adjacent highlights represent transition periods.<br/>\
               &bull; Pre/post confinement phases appear in white.<br/>\
@@ -138,7 +147,7 @@ TEXT_NOTES   ='<b>Notes:</b><br/>\
 ### Functions
 
 # the function that we are plotting
-def get_data(x, pop, n0, period, incubation, d1, d2, tr1, tr2, b1, b2,b3, tmax, dr, prog_change ):
+def get_data(x, pop, n0, period, period_stdev, incubation, d1, d2, tr1, tr2, b1, b2,b3, tmax, dr, prog_change ):
 
     h  = 1
     p  = float (b1 / 10) # input is multiplied by 10 for precision on the sliders
@@ -150,9 +159,9 @@ def get_data(x, pop, n0, period, incubation, d1, d2, tr1, tr2, b1, b2,b3, tmax, 
     I  = incubation
     N0 = n0
     DR = float(dr/100) # input is in percentage
-    M =  pop*1000000   # input is in millions
+    M  =  pop*1000000   # input is in millions
+    L  = period_stdev
 
-    L = T_STDEV # global var for now
     tint  = d1
     tint2 = d1 + d2
     progressive = prog_change # bool
@@ -164,16 +173,24 @@ def get_data(x, pop, n0, period, incubation, d1, d2, tr1, tr2, b1, b2,b3, tmax, 
                                                                                                                           progressive=progressive, ttime=ttime, h3=h3, p3=p3, tint2=tint2, ttime2=ttime2)
     print(str_params)
 
-    # this function is included from viraly.py
-    top_level = run_simulation ( h, p, T, L, I, h2, p2, tint, tmax, M, N0, DR, progressive, ttime, h3, p3, tint2, ttime2, True )
+    # decide which model to use based on the value of L
+    if L == 0:
+        prefer_mod4 = False
+    else:
+        prefer_mod4 = True
 
-    # dataset = [ n_history, nc_history, list(r_history), list(d_history), m_history, n_history, ra_history, da_history, rt_history ]
+    # this function is included from viraly.py
+    top_level = run_simulation ( h, p, T, L, I, h2, p2, tint, tmax, M, N0, DR, progressive, ttime, h3, p3, tint2, ttime2, True, prefer_mod4 )
+
+    # dataset from viraly.py: [ n_history, nc_history, list(r_history), list(d_history), m_history, n_history, ra_history, da_history, rt_history, na_history ]
     n_history  = top_level[0]
     nc_history = top_level[1]
     r_history  = top_level[2]
     d_history  = top_level[3]
     ra_history = top_level[6]
+    da_history = top_level[7]
     rt_history = top_level[8]
+    na_history = top_level[9]
 
     # calculate % of initial population which is immunized
     im_history = list ( numpy.array( ra_history ) * (100/M) )
@@ -185,22 +202,25 @@ def get_data(x, pop, n0, period, incubation, d1, d2, tr1, tr2, b1, b2,b3, tmax, 
     ar_stats = [ t_transmissions, t_recoveries, t_deaths ]
 
     # Active, New, Recovered, Dead, Rt, Immunized
-    return n_history, nc_history, r_history, d_history, rt_history, im_history, ar_stats
+    return n_history, nc_history, r_history, d_history, rt_history, im_history, na_history, ra_history, da_history, ar_stats
 
 # callback function dor updating the data
 def update_data(attrname, old, new):
 
     # Generate the new curve with the slider values
     x = np.linspace(0, DAYS, DAYS)
-    y1, y2, y3, y4, y5, y6, ar_stats = get_data(x, population.value, iinfections.value, period.value, incubation.value, duration1.value, duration2.value, transition1.value, transition2.value, beta1.value, beta2.value, beta3.value, DAYS, drate.value, True )
+    y1, y2, y3, y4, y5, y6, y7, y8, y9, ar_stats = get_data(x, population.value, iinfections.value, period.value, period_stdev.value, incubation.value, duration1.value, duration2.value, transition1.value, transition2.value, beta1.value, beta2.value, beta3.value, DAYS, drate.value, True )
 
     # Only the global variable data sources need to be updated
-    source1.data = dict(x=x, y=y1)
-    source2.data = dict(x=x, y=y2)
-    source3.data = dict(x=x, y=y3)
-    source4.data = dict(x=x, y=y4)
-    source5.data = dict(x=x, y=y5)
-    source6.data = dict(x=x, y=y6)
+    source_active.data = dict(x=x, y=y1)
+    source_new.data    = dict(x=x, y=y2)
+    source_rec.data    = dict(x=x, y=y3)
+    source_dead.data   = dict(x=x, y=y4)
+    source_rt.data     = dict(x=x, y=y5)
+    source_im.data     = dict(x=x, y=y6)
+    source_na.data     = dict(x=x, y=y7)
+    source_ra.data     = dict(x=x, y=y8)
+    source_da.data     = dict(x=x, y=y9)
 
     transition1_begin = duration1.value
     transition1_end   = transition1_begin + transition1.value
@@ -242,7 +262,8 @@ def reset_data():
 population  = Slider(title=POP_LABEL, value=POP_START, start=POP_MIN, end=POP_MAX, step=POP_STEP)
 iinfections = Slider(title=IIF_LABEL, value=IIF_START, start=IIF_MIN, end=IIF_MAX, step=1)
 
-period = Slider(title=T_LABEL, value=T_START, start=T_MIN, end=T_MAX, step=1)
+period       = Slider(title=T_LABEL,       value=T_START,       start=T_MIN,       end=T_MAX,       step=1)
+period_stdev = Slider(title=T_STDEV_LABEL, value=T_STDEV_START, start=T_STDEV_MIN, end=T_STDEV_MAX, step=1)
 
 incubation = Slider(title=I_LABEL, value=I_START, start=I_MIN, end=I_MAX, step=1)
 
@@ -268,7 +289,7 @@ notes   = Div(text='', width=TEXT_WIDTH)
 
 # Assign widgets to the call back function
 # updates are on value_throtled because this is too slow for realtime updates
-for w in [population, iinfections, period, incubation, duration1, duration2, transition1, transition2, beta1, beta2, beta3, drate ]:
+for w in [population, iinfections, period, period_stdev, incubation, duration1, duration2, transition1, transition2, beta1, beta2, beta3, drate ]:
     w.on_change('value_throttled', update_data)
 
 # reset button call back
@@ -276,15 +297,18 @@ button.on_click(reset_data)
 
 # initial plot
 x = np.linspace(0, DAYS, DAYS)
-y1, y2, y3, y4, y5, y6, ar_stats = get_data(x, population.value, iinfections.value, period.value, incubation.value, duration1.value, duration2.value, transition1.value, transition2.value, beta1.value, beta2.value, beta3.value, DAYS, drate.value, True )
+y1, y2, y3, y4, y5, y6, y7, y8, y9, ar_stats = get_data(x, population.value, iinfections.value, period.value, period_stdev.value, incubation.value, duration1.value, duration2.value, transition1.value, transition2.value, beta1.value, beta2.value, beta3.value, DAYS, drate.value, True )
 
 # Active, New, Recovered, Dead, Rt, % Immunine
-source1 = ColumnDataSource(data=dict(x=x, y=y1))
-source2 = ColumnDataSource(data=dict(x=x, y=y2))
-source3 = ColumnDataSource(data=dict(x=x, y=y3))
-source4 = ColumnDataSource(data=dict(x=x, y=y4))
-source5 = ColumnDataSource(data=dict(x=x, y=y5))
-source6 = ColumnDataSource(data=dict(x=x, y=y6))
+source_active = ColumnDataSource(data=dict(x=x, y=y1))
+source_new    = ColumnDataSource(data=dict(x=x, y=y2))
+source_rec    = ColumnDataSource(data=dict(x=x, y=y3))
+source_dead   = ColumnDataSource(data=dict(x=x, y=y4))
+source_rt     = ColumnDataSource(data=dict(x=x, y=y5))
+source_im     = ColumnDataSource(data=dict(x=x, y=y6))
+source_na     = ColumnDataSource(data=dict(x=x, y=y7))
+source_ra     = ColumnDataSource(data=dict(x=x, y=y8))
+source_da     = ColumnDataSource(data=dict(x=x, y=y9))
 
 # plot 1
 
@@ -298,7 +322,7 @@ plot.yaxis.axis_label = PLOT_Y_LABEL
 plot.add_tools(hover)
 plot.toolbar.active_inspect = None
 
-plot.line('x', 'y', source=source1, line_width=PLOT_LINE_WIDTH, line_alpha=PLOT_LINE_ALPHA, line_color=PLOT_LINE_ACTIVE_COLOR, legend_label='Active' )
+plot.line('x', 'y', source=source_active, line_width=PLOT_LINE_WIDTH, line_alpha=PLOT_LINE_ALPHA, line_color=PLOT_LINE_ACTIVE_COLOR, legend_label='Active' )
 
 # plot 2
 
@@ -311,9 +335,8 @@ plot2 = figure(plot_height=PLOT_HEIGHT, plot_width=PLOT_WIDTH, title=PLOT2_TITLE
 plot2.xaxis.axis_label = PLOT_X_LABEL
 plot2.yaxis.axis_label = PLOT_Y_LABEL
 
-plot2.line('x', 'y', source=source2, line_width=PLOT_LINE_WIDTH, line_alpha=PLOT_LINE_ALPHA, line_color=PLOT_LINE_NEW_COLOR,       legend_label='New' )
-plot2.line('x', 'y', source=source3, line_width=PLOT_LINE_WIDTH, line_alpha=PLOT_LINE_ALPHA, line_color=PLOT_LINE_RECOVERED_COLOR, legend_label='Recovered')
-plot2.line('x', 'y', source=source4, line_width=PLOT_LINE_WIDTH, line_alpha=PLOT_LINE_ALPHA, line_color=PLOT_LINE_DEAD_COLOR,      legend_label='Dead')
+plot2.line('x', 'y', source=source_new, line_width=PLOT_LINE_WIDTH, line_alpha=PLOT_LINE_ALPHA, line_color=PLOT_LINE_NEW_COLOR,       legend_label='New cases' )
+plot2.line('x', 'y', source=source_rec, line_width=PLOT_LINE_WIDTH, line_alpha=PLOT_LINE_ALPHA, line_color=PLOT_LINE_RECOVERED_COLOR, legend_label='Recoveries')
 plot2.add_tools(hover2)
 plot.toolbar.active_inspect = None
 
@@ -330,7 +353,7 @@ plot3.yaxis.axis_label = PLOT_Y_LABEL2
 plot3.add_tools(hover3)
 plot3.toolbar.active_inspect = None
 
-plot3.line('x', 'y', source=source5, line_width=PLOT_LINE_WIDTH, line_alpha=PLOT_LINE_ALPHA, line_color=PLOT_LINE_ACTIVE_COLOR, legend_label='Rt' )
+plot3.line('x', 'y', source=source_rt, line_width=PLOT_LINE_WIDTH, line_alpha=PLOT_LINE_ALPHA, line_color=PLOT_LINE_ACTIVE_COLOR, legend_label='Rt' )
 
 # plot 4
 
@@ -345,8 +368,52 @@ plot4.yaxis.axis_label = PLOT_Y_LABEL2
 plot4.add_tools(hover4)
 plot4.toolbar.active_inspect = None
 
-plot4.line('x', 'y', source=source6, line_width=PLOT_LINE_WIDTH, line_alpha=PLOT_LINE_ALPHA, line_color=PLOT_LINE_ACTIVE_COLOR, legend_label='% Immune' )
+plot4.line('x', 'y', source=source_im, line_width=PLOT_LINE_WIDTH, line_alpha=PLOT_LINE_ALPHA, line_color=PLOT_LINE_ACTIVE_COLOR, legend_label='% Immune' )
 plot4.legend.location = 'bottom_right'
+
+# plot 5
+
+hover5 = HoverTool(tooltips=[ (PLOT_X_LABEL, "$index"), (PLOT_Y_LABEL, "@y{0}")], mode="vline" )
+hover5.point_policy='snap_to_data'
+hover5.line_policy='nearest'
+
+plot5 = figure(plot_height=PLOT_HEIGHT, plot_width=PLOT_WIDTH, title=PLOT5_TITLE, tools=PLOT_TOOLS, x_range=[0, DAYS], )
+plot5.xaxis.axis_label = PLOT_X_LABEL
+plot5.yaxis.axis_label = PLOT_Y_LABEL
+plot5.add_tools(hover5)
+plot5.toolbar.active_inspect = None
+
+plot5.line('x', 'y', source=source_dead, line_width=PLOT_LINE_WIDTH, line_alpha=PLOT_LINE_ALPHA, line_color=PLOT_LINE_DEAD_COLOR, legend_label='Dead' )
+
+# plot 6
+
+# using mode="mouse" because the vline mode produces overlapping tooltips when multiple lines are used
+hover6 = HoverTool(tooltips=[ (PLOT_X_LABEL, "$index"), (PLOT_Y_LABEL, "@y{0}")], mode="mouse" )
+hover6.point_policy='snap_to_data'
+hover6.line_policy='nearest'
+
+plot6 = figure(plot_height=PLOT_HEIGHT, plot_width=PLOT_WIDTH, title=PLOT6_TITLE, tools=PLOT_TOOLS, x_range=[0, DAYS],)
+plot6.xaxis.axis_label = PLOT_X_LABEL
+plot6.yaxis.axis_label = PLOT_Y_LABEL
+
+plot6.line('x', 'y', source=source_na, line_width=PLOT_LINE_WIDTH, line_alpha=PLOT_LINE_ALPHA, line_color=PLOT_LINE_NEW_COLOR,       legend_label='Cases' )
+plot6.line('x', 'y', source=source_ra, line_width=PLOT_LINE_WIDTH, line_alpha=PLOT_LINE_ALPHA, line_color=PLOT_LINE_RECOVERED_COLOR, legend_label='Recoveries')
+plot6.legend.location = 'bottom_right'
+plot6.add_tools(hover6)
+plot.toolbar.active_inspect = None
+
+hover7 = HoverTool(tooltips=[ (PLOT_X_LABEL, "$index"), (PLOT_Y_LABEL, "@y{0}")], mode="vline" )
+hover7.point_policy='snap_to_data'
+hover7.line_policy='nearest'
+
+plot7 = figure(plot_height=PLOT_HEIGHT, plot_width=PLOT_WIDTH, title=PLOT7_TITLE, tools=PLOT_TOOLS, x_range=[0, DAYS], )
+plot7.xaxis.axis_label = PLOT_X_LABEL
+plot7.yaxis.axis_label = PLOT_Y_LABEL
+plot7.add_tools(hover7)
+plot7.toolbar.active_inspect = None
+
+plot7.line('x', 'y', source=source_da, line_width=PLOT_LINE_WIDTH, line_alpha=PLOT_LINE_ALPHA, line_color=PLOT_LINE_DEAD_COLOR, legend_label='Deaths' )
+plot7.legend.location = 'bottom_right'
 
 # highlight phases with boxes
 transition1_begin = duration1.value
@@ -376,6 +443,18 @@ plot4.add_layout(transition1_box)
 plot4.add_layout(confinement_box)
 plot4.add_layout(transition2_box)
 
+plot5.add_layout(transition1_box)
+plot5.add_layout(confinement_box)
+plot5.add_layout(transition2_box)
+
+plot6.add_layout(transition1_box)
+plot6.add_layout(confinement_box)
+plot6.add_layout(transition2_box)
+
+plot7.add_layout(transition1_box)
+plot7.add_layout(confinement_box)
+plot7.add_layout(transition2_box)
+
 # misc text
 intro.text    = TEXT_INTRO
 summary.text  = TEXT_SUMMARY
@@ -386,10 +465,10 @@ notes.text    = TEXT_NOTES
 
 # Set up layouts and add to document
 notespacer = Spacer(width=TEXT_WIDTH, height=10, width_policy='auto', height_policy='fixed')
-inputs = column(intro, population, iinfections, period, incubation, duration1, transition1, duration2, transition2, beta1, beta2, beta3, drate, button, summary, stats, notespacer, notes)
+inputs = column(intro, population, iinfections, period, period_stdev, incubation, duration1, transition1, duration2, transition2, beta1, beta2, beta3, drate, button, summary, stats, notespacer, notes)
 
 curdoc().title = PAGE_TITLE
 
 # useful for mobile scrolling on the left side
-leftmargin = Spacer(width=80, height=400, width_policy='fixed', height_policy='auto')
-curdoc().add_root( row(leftmargin,inputs, column(plot, plot2), column(plot3, plot4)) )
+leftmargin = Spacer(width=LMARGIN_WIDTH, height=400, width_policy='fixed', height_policy='auto')
+curdoc().add_root( row(leftmargin,inputs, column(plot, plot2, plot5), column(plot4, plot6, plot7), column(plot3)) )
