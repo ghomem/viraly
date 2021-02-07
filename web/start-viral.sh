bf10f0ef45b9ee9726897bfb497975c9ef076170
Gustavo Homem <gustavo@angulosolido.pt>
2021-02-07 21:29:14 +0000

    puppetize origin

--- web/start-viral.sh
+++ web/start-viral.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 MYDIR=`dirname $0`
+MYORIGIN="$1"
 
 cd $MYDIR
-bokeh serve --disable-index --allow-websocket-origin=lo.gic.li viral.py viral2.py viral-long.py viral-staging.py viral-marketing.py viral-simple.py hw.py
+bokeh serve --disable-index --allow-websocket-origin=$MYORIGIN viral.py viral2.py viral-long.py viral-staging.py viral-marketing.py viral-simple.py hw.py
