d545b79185d6305d96e0302f2eec15641326cbaf
Gustavo Homem <gustavo@angulosolido.pt>
2021-02-07 22:00:10 +0000

    allow auto origin including staging

--- web/start-viral.sh
+++ web/start-viral.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 MYDIR=`dirname $0`
-MYORIGIN="$1"
+
+HOSTNAME_COMP=`hostname | cut -d '-' -f 1`
+MYORIGIN=`hostname -f`
+MYORIGIN_STAGING=$HOSTNAME_COMP.staging.`hostname -d`
 
 cd $MYDIR
-bokeh serve --disable-index --allow-websocket-origin=$MYORIGIN viral.py viral2.py viral-long.py viral-staging.py viral-marketing.py viral-simple.py hw.py
+bokeh serve --disable-index --allow-websocket-origin=$MYORIGIN --allow-websocket-origin=$MYORIGIN_STAGING viral.py viral2.py viral-long.py viral-staging.py viral-marketing.py viral-simple.py hw.py
+
