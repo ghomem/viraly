52c7be02dcef260d86fc4ccf7952cadce31dc2da
Gustavo Homem <gustavo@angulosolido.pt>
2021-05-14 12:32:32 +0100

    simulate Rt shock due to increased contacts

--- web/start-viral.sh
+++ web/start-viral.sh
@@ -7,5 +7,5 @@ MYORIGIN=`hostname -f`
 MYORIGIN_STAGING=$HOSTNAME_COMP.staging.`hostname -d`
 
 cd $MYDIR
-bokeh serve --disable-index --allow-websocket-origin=$MYORIGIN --allow-websocket-origin=$MYORIGIN_STAGING viral.py viral2.py viral-long.py viral-staging.py viral-marketing.py viral-simple.py hw.py
+bokeh serve --disable-index --allow-websocket-origin=$MYORIGIN --allow-websocket-origin=$MYORIGIN_STAGING viral.py viral2.py viral-long.py viral-staging.py viral-marketing.py viral-simple.py viral-xmas.py hw.py
 
